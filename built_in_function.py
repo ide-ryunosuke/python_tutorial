79224771ed7258e554ee5782236782f6671596c6
ide-ryunosuke <r.i1996423@gmail.com>
2019-06-23 22:53:56 +0900

    python_built_in_function

---
+++ built_in_function.py
@@ -0,0 +1,21 @@
+# 文字数を数える
+length = len("Python")
+# >> 6
+print(length)
+
+
+# 整数を文字列に変換する
+string = str(100)
+# >> '100'
+print(string)
+
+# 文字列を整数に変換する
+intger = int("1")
+# >> 1
+print(intger)
+
+
+# 浮動小数点数オブジェクトを返す
+float = float(100)
+# >> 100.0
+print(float)
